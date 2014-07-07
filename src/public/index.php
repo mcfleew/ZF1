13710f610f02e866aa63a24d66882f274523d54f
mcfleew <fleury.timbe@gmail.com>
2014-07-07 14:49:21 +0200

    Initialisation

---
+++ src/public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(dirname(__DIR__)));
+define('SRC_PATH', ROOT_PATH . DS . 'src');
+define('PUBLIC_PATH', ROOT_PATH . DS . 'public');
+define('VENDOR_PATH', ROOT_PATH . DS . 'vendor');
+
+echo ROOT_PATH;
+
